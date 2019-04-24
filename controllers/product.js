b20ed27b79c594b977fbbf8b0ea1cabb4699169c
Diego Davalos <diego.davalos@gmail.com>
2019-04-24 17:09:44 -0400

    Controladores
    Controladores de producto.

---
+++ controllers/product.js
@@ -0,0 +1,43 @@
+var Product = require('../models/product');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Felicidades desde el Test de prueba!');
+};
+
+exports.product_create = function (req, res) {
+    var product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Producto Creado satisfactoriamente')
+    })
+};
+
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Producto Actualizado.');
+    });
+};
+
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Eliminacion satisfactoria!');
+    })
+};
